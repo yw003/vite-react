cd10ce9fce5e0e65313d0f62db469b3add88b890
yw <873821773@qq.com>
2023-07-08 20:25:33 +0800

    “配置绝对路径”

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 //样式初始化
 import "reset-css";
 //全局样式
-import './assets/styles/global.scss'
+import "@/assets/styles/global.scss";
 import App from "./App.tsx";
 // import './index.css'
 
